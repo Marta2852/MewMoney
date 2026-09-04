f7b4522503cac74a9b60526f8c392f5c49098ec8
Marta2852 <ipa23.m.dzerve@vtdt.edu.lv>
2026-09-04 11:59:40 +0300

    Datubāze done

---
+++ app/Models/Account.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Account extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'account_name',
+        'balance'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Models/SavingsGoal.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SavingsGoal extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'goal_name',
+        'target_amount',
+        'current_amount',
+        'target_date'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
